7cf5f44a38f9ef88f8b3a0d064a708e76d00ac3c
Giovanni-hash-creator <giovannig2358797@gmail.com>
2026-02-25 12:53:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("M1").onclick = function() {
+  alert("Copia este link y ponelo en el buscador de Google : https://discord.gg/2z6mXBtn");}
+document.getElementById("M2").onclick = function() {
+  alert("Contamos con 73 personas exactamente");}
+  document.getElementById("M3").onclick = function() {
+  alert("Tenemos una media activa de 17 personas, es decir, siempre somos 17 personas roleando en conjunto. Cuando hay eventos importantes llegamos a ser mas de 20 personas.");}
+  document.getElementById("M4").onclick = function() {
+  alert("Roleamos en Ecuador RP y Jalisco RP. Se espera tener mas servidores para rolear.");}
+  document.getElementById("M5").onclick = function() {
+  alert("Copia este codigo y ponelo en el buscador de google para que te aparezca la invitacion al servidor : https://discord.gg/nrCTm5DEh");}
+  alert("Bienvenido a la pagina web oficial de Mandato Abismal");
+ 
